e2e12d2251ac1393ca349f693aa42178456c9aa7
Obisedor <obisedor1@outlook.com>
2024-10-15 16:06:27 +0200

    Update v1_ThemeSettings.lua

--- v1_ThemeSettings.lua
+++ v1_ThemeSettings.lua
@@ -1,1 +1,39 @@
+local BonfireTheme = {
+    Transparency = 0.60,
+    ItemEsp = {
+        Enabled = true
+        Color = Color3.fromRGB(255, 255, 255),
+        IgnoreItems = {"Lockpick", "Plate"}
+    }
+    PlayerEsp = {
+        Enabled = true,
+        Color = Color3.fromRGB(255, 255, 255),
+        ShowHealth = true,
+        ShowMisc = true,
+        BackpackSpy = true
+    },
+    PrinterEsp = {
+        Enabled = false,
+        Color = Color3.fromRGB(255, 255, 255),
+    }
+}
 
+local EfficientTheme = {
+    Transparency = 0.45,
+    ItemEsp = {
+        Enabled = true,
+        Color = Color3.fromRGB(255, 255, 255),
+        IgnoreItems = {"Lockpick", "Plate"}
+    },
+    PlayerEsp = {
+        Enabled = true,
+        Color = Color3.fromRGB(255, 255, 255),
+        ShowHealth = true,
+        ShowMisc = true,
+        BackpackSpy = false
+    },
+    PrinterEsp = {
+        Enabled = false,
+        Color = Color3.fromRGB(255, 255, 255),
+    }
+}
