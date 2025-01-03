24d523cc580ecc46f8e27af06e49490ffa73f543
Obisedor <obisedor1@outlook.com>
2025-01-03 19:18:40 +0100

    Create OutdatedLoadstring.lua

---
+++ OptionalDepencies/OutdatedLoadstring.lua
@@ -0,0 +1,103 @@
+local Module = {}
+
+--[[ local Information = {
+    ["ScriptName"] = "",
+    ["NewLoadstring"] = "",
+    ["NewLoadstringLink"]
+} ]]--
+
+
+function Module.CreateOutdatedLoadstringNotification(Information)
+
+    local AnomicVanguard_OutdatedLoadstring = Instance.new("ScreenGui")
+    local InfoText = Instance.new("TextLabel")
+    local UICorner = Instance.new("UICorner")
+    local LoadNew = Instance.new("TextButton")
+    local UICorner_2 = Instance.new("UICorner")
+    local Ignore = Instance.new("TextButton")
+    local UICorner_3 = Instance.new("UICorner")
+    local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+    local Blur = Instance.new("Blur")
+
+    --Properties:
+
+    AnomicVanguard_OutdatedLoadstring.Name = "AnomicVanguard_OutdatedLoadstring"
+    AnomicVanguard_OutdatedLoadstring.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    AnomicVanguard_OutdatedLoadstring.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+    InfoText.Name = "InfoText"
+    InfoText.Parent = AnomicVanguard_OutdatedLoadstring
+    InfoText.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+    InfoText.BackgroundTransparency = 0.500
+    InfoText.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    InfoText.BorderSizePixel = 0
+    InfoText.Position = UDim2.new(0.235872239, 0, 0.270100504, 0)
+    InfoText.Size = UDim2.new(0.528255522, 0, 0.282663316, 0)
+    InfoText.Font = Enum.Font.SourceSans
+    InfoText.Text = "Anomic Vanguard | You are using the wrong loadstring of " .. Information.ScriptName .. "! Please, use the new loadstring to continue using this script. Attempted script: " .. Information.ScriptName .. " New loadstring: " .. Information.NewLoadstring
+    InfoText.TextColor3 = Color3.fromRGB(253, 253, 253)
+    InfoText.TextScaled = true
+    InfoText.TextSize = 14.000
+    InfoText.TextWrapped = true
+
+    UICorner.CornerRadius = UDim.new(0.200000003, 0)
+    UICorner.Parent = InfoText
+
+    LoadNew.Name = "LoadNew"
+    LoadNew.Parent = AnomicVanguard_OutdatedLoadstring
+    LoadNew.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+    LoadNew.BackgroundTransparency = 0.500
+    LoadNew.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    LoadNew.BorderSizePixel = 0
+    LoadNew.Position = UDim2.new(0.388206393, 0, 0.57914567, 0)
+    LoadNew.Size = UDim2.new(0.22194922, 0, 0.0628140718, 0)
+    LoadNew.Font = Enum.Font.SourceSans
+    LoadNew.Text = "Load new loadstring"
+    LoadNew.TextColor3 = Color3.fromRGB(255, 255, 255)
+    LoadNew.TextScaled = true
+    LoadNew.TextSize = 14.000
+    LoadNew.TextWrapped = true
+
+    UICorner_2.CornerRadius = UDim.new(0.200000003, 0)
+    UICorner_2.Parent = LoadNew
+
+    Ignore.Name = "Ignore"
+    Ignore.Parent = AnomicVanguard_OutdatedLoadstring
+    Ignore.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+    Ignore.BackgroundTransparency = 0.500
+    Ignore.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Ignore.BorderSizePixel = 0
+    Ignore.Position = UDim2.new(0.417690426, 0, 0.665829122, 0)
+    Ignore.Size = UDim2.new(0.163800165, 0, 0.0628140718, 0)
+    Ignore.Font = Enum.Font.SourceSans
+    Ignore.Text = "Ignore"
+    Ignore.TextColor3 = Color3.fromRGB(255, 255, 255)
+    Ignore.TextScaled = true
+    Ignore.TextSize = 14.000
+    Ignore.TextWrapped = true
+
+    UICorner_3.CornerRadius = UDim.new(0.200000003, 0)
+    UICorner_3.Parent = Ignore
+
+    UIAspectRatioConstraint.Parent = AnomicVanguard_OutdatedLoadstring
+    UIAspectRatioConstraint.AspectRatio = 1.534
+
+    Blur.Parent = game.Lighting
+    Blur.Size = 20
+
+    local function CloseUI()
+        Blur:Destroy()
+        AnomicVanguard_OutdatedLoadstring:Destroy()
+    end
+
+    Ignore.MouseButton1Click:Connect(function()
+        CloseUI()
+    end)
+    LoadNew.MouseButton1Click:Connect(function()
+        CloseUI()
+        loadstring(game:HttpGet(Information.NewLoadstringLink))()
+    end)
+
+end
+
+return Module
