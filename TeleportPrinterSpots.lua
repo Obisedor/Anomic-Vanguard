89fe943c9b9f5573cc86812bd8267d682303f9f2
Obisedor <obisedor1@outlook.com>
2025-02-21 12:23:13 +0100

    Delete TeleportPrinterSpots.lua

--- TeleportPrinterSpots.lua
+++
@@ -1,7 +0,0 @@
-local OutdatedLoadstringModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Obisedor/Anomic-Vanguard/refs/heads/main/OptionalDepencies/OutdatedLoadstring.lua"))()
-
-OutdatedLoadstringModule:CreateOutdatedLoadstringNotification({
-    ScriptName = "Printing spots teleport menu",
-    NewLoadstring = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/Obisedor/Anomic-Vanguard/refs/heads/main/Printing%20spots%20teleport%20menu.lua"))()',
-    NewLoadstringLink = "https://raw.githubusercontent.com/Obisedor/Anomic-Vanguard/refs/heads/main/Printing%20spots%20teleport%20menu.lua"
-})
